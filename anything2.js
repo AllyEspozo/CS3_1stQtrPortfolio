70d72d093fd4f7afdcba8a4ae861b765e52c2a8f
sammr123a <b2029stkaustria@pshs.edu.ph>
2025-10-10 15:53:48 +0800

    test

---
+++ anything2.js
@@ -0,0 +1,19 @@
+for (let i = 2; i <= 20; i+=2){
+    console.log(i);
+}
+
+var i = 1
+while(i <= 3){
+    console.log(i);
+    i++;
+}
+    
+i = 1;
+    do {
+        console.log(i);
+        i++;
+    } while (i <=3);
+
+    return 0;
+
+    
