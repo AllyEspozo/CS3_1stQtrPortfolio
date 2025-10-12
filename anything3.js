7c1de488a32e4142dfa02546f423f9c9474258dd
AllyEspozo <b2029alpespinoza@pshs.edu.ph>
2025-10-12 13:50:51 +0800

    Removing Unused Files

--- anything3.js
+++
@@ -1,8 +0,0 @@
-    function squareNumber() {
-      const input = prompt("Enter a number:");
-      const num = Number(input);
-      const result = num * num;
-      alert(`The square of ${num} is ${result}`);
-    }
-    squareNumber();
-    
